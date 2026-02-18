5edb1e7e91be38a43eaa724739463c6ab5ea9a74
Roman Spanek <roman.spanek@tul.cz>
2026-02-18 11:41:38 +0100

    modification of exception handling

--- Functions.java
+++ Functions.java
@@ -2,9 +2,11 @@ class Functions {
     private final int LOWEST = 10;
     private final int HIGHEST = 100;
     int add(int a, int b) {
-        if (a < LOWEST || b > HIGHEST) {
-            System.out.println("Input values are outside allowed range.");
-        }
+	if (a < LOWEST || a > HIGHEST ||
+            b < LOWEST || b > HIGHEST) {
+            System.out.println("Values must be between 10 and 100");
+            return -1;
+	}
         return a + b;
     }
 }

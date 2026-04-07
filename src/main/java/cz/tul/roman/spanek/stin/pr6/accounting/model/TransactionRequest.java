5805e753dec7cb5dfbe424f31e3f71ea09d3a76b
Roman Spanek <roman.spanek@tul.cz>
2026-04-07 22:32:44 +0200

    pr08-ci-cd pipelines

--- src/main/java/cz/tul/roman/spanek/stin/pr6/accounting/model/TransactionRequest.java
+++ src/main/java/cz/tul/roman/spanek/stin/pr6/accounting/model/TransactionRequest.java
@@ -11,20 +11,9 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TransactionRequest {
-
-    /** Target account number. */
     private String accountNumber;
-
-    /**
-     * Amount of the operation.
-     * Positive value = deposit, negative value = withdrawal.
-     */
     private BigDecimal amount;
-
-    /** Currency of the transaction. */
     private Currency currency;
-
-    /** Date when the operation takes place. */
     private LocalDate operationDate;
 }
 
